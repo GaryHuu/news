dd745fc827106e8e2fc1d420f8bd04fb8bfccea6
GaryHuu <nguyenhuu220800@gmail.com>
2021-04-26 21:02:25 +0700

    hi

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const contact = document.querySelector("#contact");
+contact.addEventListener("click", () => {
+  let infoContact = contact.querySelector(".info-contact");
+  if (infoContact.classList.contains("d-n")) {
+    infoContact.classList.remove("d-n");
+  } else {
+    infoContact.classList.add("d-n");
+  }
+});
+
+const navSearch = document.querySelector(".nav__search");
+const navSearchContent = document.querySelector(".nav__search-content");
+navSearch.addEventListener("click", () => {
+  navSearch.querySelector("input").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+  if (navSearchContent.classList.contains("nav__search--show")) {
+    navSearchContent.classList.remove("nav__search--show");
+  } else {
+    navSearchContent.classList.add("nav__search--show");
+  }
+});
+
+const clickScrollTop = document.querySelector(".arrow-to-top");
+clickScrollTop.addEventListener("click", () => {
+  window.scroll({ top: 0, behavior: "smooth" });
+});
